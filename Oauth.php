adf51ce1066c1865ddc98101c518a22b2aa3d4e5
judopro <yesildal@gmail.com>
2019-05-29 18:32:02 -0700

    Updated OAuth.php to support SHA256 signature
    SHA1 is obsolete now, many API endpoints will not support SHA1 or plaintext HTTP Authorizations going forward.

--- Oauth.php
+++ Oauth.php
@@ -106,6 +106,30 @@ abstract class OAuthSignatureMethod {
 }
 
 /**
+ * The HMAC-SHA256 signature method uses the HMAC-SHA256 signature algorithm 
+ */
+class OAuthSignatureMethod_HMAC_SHA256 extends OAuthSignatureMethod {
+  function get_name() {
+    return "HMAC-SHA256";
+  }
+
+  public function build_signature($request, $consumer, $token) {
+    $base_string = $request->get_signature_base_string();
+    $request->base_string = $base_string;
+
+    $key_parts = array(
+      $consumer->secret,
+      ($token) ? $token->secret : ""
+    );
+
+    $key_parts = OAuthUtil::urlencode_rfc3986($key_parts);
+    $key = implode('&', $key_parts);
+
+    return base64_encode(hash_hmac('sha256', $base_string, $key, true));
+  }
+}
+
+/**
  * The HMAC-SHA1 signature method uses the HMAC-SHA1 signature algorithm as defined in [RFC2104] 
  * where the Signature Base String is the text and the key is the concatenated values (each first 
  * encoded per Parameter Encoding) of the Consumer Secret and Token Secret, separated by an '&' 
